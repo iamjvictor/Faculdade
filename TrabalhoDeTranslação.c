8db1c9ebacc54859133865e75fba444da302d9a1
Joao Victor <joao-victor_07@outlook.com>
2021-09-24 19:54:15 -0300

    Comecei o código

--- TrabalhoDeTranslação.c
+++ TrabalhoDeTranslação.c
@@ -1,1 +1,25 @@
 //Trabalho de translação de vetores
+  #include <stdio.h>
+  #include <stdlib.h>
+
+int main (){
+    int n, i,vx[5] , vy[5], cx, cy;
+   
+   
+
+        for (i=0; i<5; i++){
+            printf("Digite x");
+            scanf("%d", &vx[i]);
+            printf("Digite y");
+            scanf("%d", &vy[i]);
+        }
+        
+        for (i=0; i<5; i++){
+            printf("%d, %d \n", vx[i], vy[i]);
+            
+            
+        }
+
+        //primeira parte
+return (0);
+}
